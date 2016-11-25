e605f8dbaef7c2c437f018737c5df5b26d031fd0
UrbanSkurk <UrbanSkurk@HELL>
2016-11-25 11:50:34 +0100

    sg

--- RealGetter.Persons/Person.cs
+++ RealGetter.Persons/Person.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace RealGetter.Lib
 {
-    public abstract class Person
+    public abstract class Person : IPerson
     {
         //public int StudentId { get; set; }
         public string FirstName { get; set; }
