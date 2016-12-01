0aa469b8d3fc39c702a2af13750c42618f94d65e
Urbanskurk <Urbanskurk@HELL>
2016-12-02 00:23:21 +0100

    dg

--- RealGetter.Persons/ToolBox/Sorting.cs
+++ RealGetter.Persons/ToolBox/Sorting.cs
@@ -34,32 +34,53 @@ namespace RealGetter.Lib
             
         }
 
+        public void Grouper<T>(List<T> t, uint groupSize, uint startingGroupNumber) where T : IGroupNumber
+        {
+            if (groupSize < 1) throw new IndexOutOfRangeException("There must be at least one group");
+            int currentGroupNumber = (int)startingGroupNumber;
+            int groupCounter = 0;
+
+            for (int i = 0; i < t.Count; i++)
+            {
+                if (groupCounter >= groupSize)
+                {
+                    currentGroupNumber += 1;
+                    groupCounter = 0;
+                }
+
+                t[i].GroupNumber = currentGroupNumber;
+                groupCounter += 1;
+            }
+
+
+        }
+
         public void Randomizer<T>(IList<T> t) 
         {
             IList<T> input = new List<T>(t);
             IList<T> output = new List<T>();
-            Random rand = new Random();
             int seed;
 
             //int counter = input.Count;
             //for (int i = counter; i > 0; i--)
             //{
-            //    seed = rand.Next(input.Count);
+            //    seed = new Random().Next(counter);
             //    output.Add(input[seed]);
             //    input.RemoveAt(seed);
             //    counter--;
+            //    t = output;
             //}
 
 
             while (input.Count > 0)
             {
-                seed = rand.Next(input.Count);
+                seed = new Random().Next(input.Count);
                 output.Add(input[seed]);
                 input.RemoveAt(seed);
             }
 
             t = output;
-            
+
         }
     }
 }
