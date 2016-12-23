9c2cc059f635aa2f4977ec37a77b5bb9d86113fe
Urbanskurk <Urbanskurk@HELL>
2016-12-23 09:17:47 +0100

    er5yu

--- RealGetter/Program.cs
+++ RealGetter/Program.cs
@@ -31,61 +31,43 @@ namespace RealGetter
 
 
 
-            //Sorting sort = new Sorting();
-            //IList<Student> lint = new List<Student>();
-            //lint.Add(new Student(1, "Casper", "", "", new StudentInfo() ));
-            //lint.Add(new Student(1, "Mikkel", "", "", new StudentInfo()));
-            //lint.Add(new Student(1, "Peter", "", "", new StudentInfo()));
-            //lint.Add(new Student(1, "Torben", "", "", new StudentInfo()));
-            //lint.Add(new Student(1, "Christian", "", "", new StudentInfo()));
-            //lint.Add(new Student(1, "Delle", "", "", new StudentInfo()));
-            //lint.Add(new Student(1, "Bølle", "", "", new StudentInfo()));
+            Sorting sort = new Sorting();
+            IList<Student> lint = new List<Student>();
+            lint.Add(new Student(1, "Casper", "", "", new StudentInfo()));
+            lint.Add(new Student(1, "Mikkel", "", "", new StudentInfo()));
+            lint.Add(new Student(1, "Peter", "", "", new StudentInfo()));
+            lint.Add(new Student(1, "Torben", "", "", new StudentInfo()));
+            lint.Add(new Student(1, "Christian", "", "", new StudentInfo()));
+            lint.Add(new Student(1, "Delle", "", "", new StudentInfo()));
+            lint.Add(new Student(1, "Bølle", "", "", new StudentInfo()));
 
-            //lint = sort.Randomizer(lint);
-            //Console.WriteLine(lint[0].ToString());
-            //Console.WriteLine(lint[1].ToString());
-            //Console.WriteLine(lint[2].ToString());
-            //Console.WriteLine(lint[3].ToString());
-            //Console.WriteLine(lint[4].ToString());
-            //Console.WriteLine(lint[5].ToString());
-            //Console.WriteLine(lint[6].ToString());
-            //Console.ReadLine();
+            lint = sort.Randomizer(lint);
+            Console.WriteLine(lint[0].ToString());
+            Console.WriteLine(lint[1].ToString());
+            Console.WriteLine(lint[2].ToString());
+            Console.WriteLine(lint[3].ToString());
+            Console.WriteLine(lint[4].ToString());
+            Console.WriteLine(lint[5].ToString());
+            Console.WriteLine(lint[6].ToString());
+            Console.ReadLine();
 
-            //sort.Grouper(lint, 2);
+            sort.Grouper(lint, 2);
 
-            //Console.WriteLine(lint[0].GroupNumber);
-            //Console.WriteLine(lint[1].GroupNumber);
-            //Console.WriteLine(lint[2].GroupNumber);
-            //Console.WriteLine(lint[3].GroupNumber);
-            //Console.WriteLine(lint[4].GroupNumber);
-            //Console.WriteLine(lint[5].GroupNumber);
-            //Console.WriteLine(lint[6].GroupNumber);
-
-
+            Console.WriteLine(lint[0].GroupNumber);
+            Console.WriteLine(lint[1].GroupNumber);
+            Console.WriteLine(lint[2].GroupNumber);
+            Console.WriteLine(lint[3].GroupNumber);
+            Console.WriteLine(lint[4].GroupNumber);
+            Console.WriteLine(lint[5].GroupNumber);
+            Console.WriteLine(lint[6].GroupNumber);
 
 
 
-            //Sorting sort = new Sorting();
-            //IList<Student> lint = new List<Student>();
-            //lint.Add(new Student(1, "Casper", "", "", ""));
-            //lint.Add(new Student(1, "Mikkel", "", "", ""));
-            //lint.Add(new Student(1, "Peter", "", "", ""));
-            //lint.Add(new Student(1, "Torben", "", "", ""));
-            //lint.Add(new Student(1, "Christian", "", "", ""));
-            //lint.Add(new Student(1, "Delle", "", "", ""));
-            //lint.Add(new Student(1, "Bølle", "", "", ""));
 
-            //con.Open();
-            //SqlCommand cmd1 = new SqlCommand("InsertPet", con);
-            //cmd1.CommandType = CommandType.StoredProcedure;
-            //cmd1.Parameters.Add("@Petname", SqlDbType.NVarChar);
-            //cmd1.Parameters["@PetName"].Value = "Kalle";
-            //cmd1.Parameters.Add(new SqlParameter("@PetType", "Kalle"));
-            //cmd1.Parameters.Add(new SqlParameter("@PetBreed", "Kalle"));
-            //cmd1.Parameters.Add(new SqlParameter("@PetDOB", DateTime.Now));
-            //cmd1.Parameters.Add(new SqlParameter("@PetWeight", 2.54m));
 
-            //cmd1.ExecuteNonQuery();
+            Console.ReadLine();
+
+            
         }
 
     }
